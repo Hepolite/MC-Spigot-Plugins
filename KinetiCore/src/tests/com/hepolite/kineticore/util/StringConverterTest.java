37d835ccc1d7a96a3f4dc50f679e3ba77f35882f
Hepolite <CW.Hepolite@hotmail.com>
2018-08-05 20:04:51 +0200

    Added string conversion helper

---
+++ KinetiCore/src/tests/com/hepolite/kineticore/util/StringConverterTest.java
@@ -0,0 +1,159 @@
+package com.hepolite.kineticore.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringConverterTest
+{
+	@Test
+	void testToBool()
+	{
+		assertEquals(true, StringConverter.toBool("true"));
+		assertEquals(false, StringConverter.toBool("false"));
+	}
+	@Test
+	void testToBools()
+	{
+		assertArrayEquals(bools(false, true, false),
+			StringConverter.toBools("whatever true false", " "));
+	}
+
+	@Test
+	void testToByte()
+	{
+		assertEquals((byte) 123, StringConverter.toByte("123"));
+		assertEquals((byte) -42, StringConverter.toByte("-42"));
+		assertEquals((byte) 0, StringConverter.toByte("invalid"));
+		assertEquals((byte) 3, StringConverter.toByte("732516", (byte) 3));
+	}
+	@Test
+	void testToBytes()
+	{
+		assertArrayEquals(bytes((byte) -6, (byte) 15, (byte) 0),
+			StringConverter.toBytes("-6 15 invalid", " "));
+	}
+
+	@Test
+	void testToDouble()
+	{
+		assertEquals(123.4, StringConverter.toDouble("123.4"));
+		assertEquals(-42.0, StringConverter.toDouble("-42"));
+		assertEquals(0.0, StringConverter.toDouble("invalid"));
+		assertEquals(3.14, StringConverter.toDouble("73.251.6", 3.14));
+	}
+	@Test
+	void testToDoubles()
+	{
+		assertArrayEquals(doubles(-6.0, 15.1, 0.0),
+			StringConverter.toDoubles("-6 15.1 invalid", " "));
+	}
+
+	@Test
+	void testToFloat()
+	{
+		assertEquals(123.4f, StringConverter.toFloat("123.4"));
+		assertEquals(-42.0f, StringConverter.toFloat("-42"));
+		assertEquals(0.0f, StringConverter.toFloat("invalid"));
+		assertEquals(3.14f, StringConverter.toFloat("73.251.6", 3.14f));
+	}
+	@Test
+	void testToFloats()
+	{
+		assertArrayEquals(floats(-6.0f, 15.1f, 0.0f),
+			StringConverter.toFloats("-6 15.1 invalid", " "));
+	}
+
+	@Test
+	void testToInt()
+	{
+		assertEquals(123, StringConverter.toInt("123"));
+		assertEquals(-42, StringConverter.toInt("-42"));
+		assertEquals(0, StringConverter.toInt("invalid"));
+		assertEquals(3, StringConverter.toInt("73.2516", 3));
+	}
+	@Test
+	void testToInts()
+	{
+		assertArrayEquals(ints(-6, 15, 0),
+			StringConverter.toInts("-6 15 invalid", " "));
+	}
+
+	@Test
+	void testToLong()
+	{
+		assertEquals(123L, StringConverter.toLong("123"));
+		assertEquals(-42L, StringConverter.toLong("-42"));
+		assertEquals(0L, StringConverter.toLong("invalid"));
+		assertEquals(3L, StringConverter.toLong("73.2516", 3L));
+	}
+	@Test
+	void testToLongs()
+	{
+		assertArrayEquals(longs(-6L, 15L, 0L),
+			StringConverter.toLongs("-6 15 invalid", " "));
+	}
+
+	@Test
+	void testToShort()
+	{
+		assertEquals((short) 123, StringConverter.toShort("123"));
+		assertEquals((short) -42, StringConverter.toShort("-42"));
+		assertEquals((short) 0, StringConverter.toShort("invalid"));
+		assertEquals((short) 3, StringConverter.toShort("732516", (short) 3));
+	}
+	@Test
+	void testToShorts()
+	{
+		assertArrayEquals(shorts((short) -6, (short) 15, (short) 0),
+			StringConverter.toShorts("-6 15 invalid", " "));
+	}
+
+	@Test
+	void testToString()
+	{
+		assertEquals("test", StringConverter.toString("test"));
+		assertEquals("whatever", StringConverter.toString(null, "whatever"));
+	}
+	@Test
+	void testToStrings()
+	{
+		assertArrayEquals(strings("Hello", "World", "!"),
+			StringConverter.toStrings("Hello World !", " "));
+	}
+
+	// ...
+
+	boolean[] bools(final boolean... values)
+	{
+		return values;
+	}
+	byte[] bytes(final byte... values)
+	{
+		return values;
+	}
+	double[] doubles(final double... values)
+	{
+		return values;
+	}
+	float[] floats(final float... values)
+	{
+		return values;
+	}
+	int[] ints(final int... values)
+	{
+		return values;
+	}
+	long[] longs(final long... values)
+	{
+		return values;
+	}
+	short[] shorts(final short... values)
+	{
+		return values;
+	}
+	String[] strings(final String... values)
+	{
+		return values;
+	}
+}
